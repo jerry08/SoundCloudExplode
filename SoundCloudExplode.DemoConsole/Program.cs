5630783702ba4bb81004bfd920008b7ceb2e0ae9
Jeremy <74879119+jerry08@users.noreply.github.com>
2022-05-25 15:46:51 -0400

    Upload

---
+++ SoundCloudExplode.DemoConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SoundCloudExplode.DemoConsole
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            Console.Title = "SoundCloudExplode Demo";
+
+            var soundCloud = new SoundCloudClient();
+
+            // Read the track/playlist url
+            Console.Write("Enter SoundCloud track/playlist URL: ");
+            var url = Console.ReadLine() ?? "";
+
+            var tracks = await soundCloud.GetTracksAsync(url);
+
+            foreach (var track in tracks)
+            {
+                await soundCloud.Download(track, $@"{Environment.CurrentDirectory}\Download\{ReplaceInvalidChars(track.Title)}.mp3");
+            }
+
+            return 0;
+        }
+
+        public static string ReplaceInvalidChars(string filename)
+        {
+            return string.Join("_", filename.Split(Path.GetInvalidFileNameChars()));
+        }
+    }
+}
