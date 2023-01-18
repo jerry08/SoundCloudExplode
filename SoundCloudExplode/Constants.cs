4f42a674a7c0c77ef112e8b980dd712861758f19
jerrry08 <jeremyyearwood99@gmail.com>
2023-01-18 13:19:01 -0400

    Added default constants

--- SoundCloudExplode/Constants.cs
+++ SoundCloudExplode/Constants.cs
@@ -1,6 +1,6 @@
 ﻿namespace SoundCloudExplode;
 
-internal class Constants
+internal static class Constants
 {
     public const string ClientId = "a3e059563d7fd3372b49b37f00a00bcf";
     //public const string ClientId = "a3dd183a357fcff9a6943c0d65664087";
@@ -9,4 +9,12 @@ internal class Constants
     public const string ResolveEndpoint = "https://api-v2.soundcloud.com/resolve";
     public const string TrackEndpoint = "https://api-v2.soundcloud.com/tracks";
     //public const string TrackEndpoint = "https://api.soundcloud.com/i1/tracks";
+
+    public const int DefaultOffset = 0;
+
+    public const int DefaultLimit = 50;
+
+    public const int MinLimit = 0;
+
+    public const int MaxLimit = 200;
 }
