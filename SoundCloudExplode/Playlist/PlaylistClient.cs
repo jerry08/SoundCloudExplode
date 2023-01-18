88370dcf0cb5ba4aaba8628a1cd1cb7acfa596eb
jerrry08 <jeremyyearwood99@gmail.com>
2023-01-18 13:16:40 -0400

    Refactor

--- SoundCloudExplode/Playlist/PlaylistClient.cs
+++ SoundCloudExplode/Playlist/PlaylistClient.cs
@@ -1,28 +1,28 @@
 ﻿using System;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Runtime.CompilerServices;
 using Newtonsoft.Json;
+using SoundCloudExplode.Bridge;
 using SoundCloudExplode.Common;
 using SoundCloudExplode.Playlist;
 using SoundCloudExplode.Exceptions;
 using SoundCloudExplode.Utils.Extensions;
-using System.Net.Http;
-using SoundCloudExplode.Bridge;
 
 namespace SoundCloudExplode.Track;
 
 /// <summary>
-/// Operations related to Soundcloud playlist.
+/// Operations related to Soundcloud playlist/album.
+/// (Note: Everything for Playlists and Albums are handled the same.)
 /// </summary>
 public class PlaylistClient
 {
     private readonly HttpClient _http;
     private readonly SoundcloudEndpoint _endpoint;
-    private readonly TrackClient _tracks;
 
     private readonly Regex PlaylistRegex = new(@"soundcloud\..+?\/(.*?)\/sets\/[a-zA-Z]+");
 
@@ -31,12 +31,10 @@ public class PlaylistClient
     /// </summary>
     public PlaylistClient(
         HttpClient http,
-        SoundcloudEndpoint endpoint,
-        TrackClient tracks)
+        SoundcloudEndpoint endpoint)
     {
         _http = http;
         _endpoint = endpoint;
-        _tracks = tracks;
     }
 
     /// <summary>
@@ -74,7 +72,7 @@ public class PlaylistClient
 
         if (autoPopulateAllTracks)
         {
-            var tracks = await GetTracksAsync(url);
+            var tracks = await GetTracksAsync(url, cancellationToken: cancellationToken);
             playlist.Tracks = tracks.ToArray();
         }
 
@@ -87,7 +85,7 @@ public class PlaylistClient
     public async IAsyncEnumerable<Batch<TrackInformation>> GetTrackBatchesAsync(
         string url,
         int offset = 0,
-        int limit = -1,
+        int limit = 0,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
         if (!IsUrlValid(url))
@@ -107,7 +105,7 @@ public class PlaylistClient
         {
             var ids = string.Join(",", chunk.Select(x => x.Id));
 
-            var response = await _http.ExecuteGetAsync($"https://api-v2.soundcloud.com/tracks?ids={ids}&limit={limit}&offset={offset}&client_id={Constants.ClientId}");
+            var response = await _http.ExecuteGetAsync($"https://api-v2.soundcloud.com/tracks?ids={ids}&limit={limit}&offset={offset}&client_id={Constants.ClientId}", cancellationToken);
             yield return Batch.Create(JsonConvert.DeserializeObject<List<TrackInformation>>(response)!);
         }
     }
@@ -118,7 +116,7 @@ public class PlaylistClient
     public IAsyncEnumerable<TrackInformation> GetTracksAsync(
         string url,
         int offset = 0,
-        int limit = -1,
+        int limit = 0,
         CancellationToken cancellationToken = default) =>
         GetTrackBatchesAsync(url, offset, limit, cancellationToken).FlattenAsync();
 }
