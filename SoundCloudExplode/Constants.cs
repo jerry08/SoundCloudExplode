a77a6347761cf2b7f86df7382ea0c9e67206519a
jerry08 <jeremyyearwood99@gmail.com>
2024-06-10 12:35:50 -0400

    Closes #2

--- SoundCloudExplode/Constants.cs
+++ SoundCloudExplode/Constants.cs
@@ -2,12 +2,11 @@
 
 internal static class Constants
 {
-    public const string ClientId = "a3e059563d7fd3372b49b37f00a00bcf";
-    //public const string ClientId = "a3dd183a357fcff9a6943c0d65664087";
-    //public const string ClientId = "eLWKwhCY4BrKxcpiyhjyr6SeHiszzUq6";
+    public const string ClientId = "ICQyLasUBASlFk0tLJ8FRmTTFc11FVBZ";
 
     public const string ResolveEndpoint = "https://api-v2.soundcloud.com/resolve";
     public const string TrackEndpoint = "https://api-v2.soundcloud.com/tracks";
+
     //public const string TrackEndpoint = "https://api.soundcloud.com/i1/tracks";
 
     public const int DefaultOffset = 0;
