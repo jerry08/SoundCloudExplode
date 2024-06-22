8411694a363cf1e359f9a7342b33d31c46c75570
jerry08 <jeremyyearwood99@gmail.com>
2024-06-22 17:13:13 -0400

    Add limitor

---
+++ SoundCloudExplode/Common/Limitor.cs
@@ -0,0 +1,16 @@
+﻿using SoundCloudExplode.Exceptions;
+
+namespace SoundCloudExplode.Common;
+
+internal static class Limitor
+{
+    public static void Validate(int limit)
+    {
+        if (limit is < Constants.MinLimit or > Constants.MaxLimit)
+        {
+            throw new SoundcloudExplodeException(
+                $"Limit must be between {Constants.MinLimit} and {Constants.MaxLimit}"
+            );
+        }
+    }
+}
